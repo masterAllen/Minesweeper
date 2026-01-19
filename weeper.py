dc6f42490e3d6d0183c371fb8c916c1811c40df9
masterAllen <allenhong2000@foxmail.com>
2026-01-19 23:35:28 +0800

    Complete all [V]

---
+++ weeper.py
@@ -0,0 +1,456 @@
+'''
+扫雷游戏类
+'''
+import os
+import sys
+import numpy as np
+from typing import Iterator
+from window_analyzer import WindowAnalyzer
+import utils
+import itertools
+import math
+import time
+
+class Constraint:
+    def __init__(self, coordinates: list):
+        # self.coordinates = tuple(coordinates)
+        # 按照顺序排序
+        coordinates_sorted = sorted(coordinates, key=lambda x: (x[0], x[1]))
+        self.coordinates = tuple(coordinates_sorted)
+
+    def __repr__(self) -> str:
+        return f'{[x for x in self.coordinates]}'
+
+    def __iter__(self) -> Iterator[tuple[int, int]]:
+        return iter(self.coordinates)
+
+    def __sub__(self, other: 'Constraint') -> 'Constraint':
+        my_coordinates_set = set(self.coordinates)
+        other_coordinates_set = set(other.coordinates)
+        return Constraint(list(my_coordinates_set - other_coordinates_set))
+
+    def is_subset(self, other: 'Constraint') -> bool:
+        return set(self.coordinates).issubset(set(other.coordinates))
+
+    def __len__(self) -> int:
+        return len(self.coordinates)
+
+    def __hash__(self) -> int:
+        return hash(self.coordinates)
+
+    def __eq__(self, other: 'Constraint') -> bool:
+        return set(self.coordinates) == set(other.coordinates)
+
+class Weeper:
+    def __init__(self, table: np.ndarray, mine_total: int) -> None:
+        self.mine_total = mine_total
+        self.mine_count = mine_total
+        self.unknown_count = None
+        self.table = table
+        
+        if table is None:
+            window_title = "Minesweeper Variants"
+            self.window_analyzer = WindowAnalyzer(window_title)
+
+    def refresh_table(self, refresh_by_screenshot: bool = True):
+        if refresh_by_screenshot:
+            screenshot = self.window_analyzer.capture_window_screenshot()
+            self.table = self.window_analyzer.parse_img_to_table(screenshot)
+
+        # 统计 unknown 数量
+        self.unknown_count = np.sum(self.table == 'unknown')
+        self.mine_count = self.mine_total - np.sum(self.table == 'mine')
+
+    def print_table(self, table: np.ndarray):
+        print(f'============ 剩余雷: {self.mine_count}，未知格: {self.unknown_count} ============')
+        for i in range(table.shape[0]):
+            print('-' * (table.shape[1] * 4 + 1))
+            for j in range(table.shape[1]):
+                print('|', end=' ')
+                if table[i, j] == 'unknown':
+                    print(' ', end=' ')
+                elif table[i, j] == 'mine':
+                    print('*', end=' ')
+                elif table[i, j] == 'question':
+                    print('?', end=' ')
+                else:
+                    print(table[i, j], end=' ')
+            print('|')
+        print('-' * (table.shape[1] * 4 + 1))
+
+    def create_table_constraints(self, table: np.ndarray, mine_count: int) -> np.ndarray:
+        constraints = dict()
+        unknown_coordinates = list()
+
+        # 先遍历所有格子，创建约束
+        for i in range(table.shape[0]):
+            for j in range(table.shape[1]):
+                if table[i, j].isdigit():
+                    coordinates, mine_count = self.create_cell_contraint(i, j, table)
+                    if len(coordinates) > 0:
+                        constraints[coordinates] = mine_count
+                if table[i, j] == 'unknown':
+                    unknown_coordinates.append((i, j))
+
+        # 全局数量也有一个约束
+        if self.mine_count < 10 and self.unknown_count < 20:
+            constraints[Constraint(unknown_coordinates)] = self.mine_count
+        for coordinates, mine_count in constraints.items():
+            print(coordinates, mine_count)
+        print('--------------------------------------------')
+
+        # 寻找约束中是否有子约束
+        while True:
+            print(f'--> Constraints NUM: {len(constraints)}')
+            if len(constraints) > 300:
+                break
+
+            new_constraints = dict()
+            for now_coordinates, now_mine_count in constraints.items():
+                for other_coordinates, other_mine_count in constraints.items():
+                    # 如果不是子集，那跳过
+                    if not now_coordinates.is_subset(other_coordinates):
+                        continue
+
+                    new_coordinates = other_coordinates - now_coordinates
+                    new_mine_count = other_mine_count - now_mine_count
+
+                    # 这里有可能 mine_count 是 0
+                    if len(new_coordinates) > 0:
+                        is_new = False
+                        if new_coordinates in constraints:
+                            if new_mine_count < constraints[new_coordinates]:
+                                is_new = True
+                        else:
+                            is_new = True
+
+                        if is_new:
+                            new_constraints[new_coordinates] = new_mine_count
+
+                    if new_mine_count < 0:
+                        raise ValueError(f'new_mine_count < 0: {new_mine_count}')
+
+            if len(new_constraints) == 0:
+                break
+
+            for new_coordinates, new_mine_count in new_constraints.items():
+                constraints[new_coordinates] = new_mine_count
+
+        return constraints
+
+    def create_cell_contraint(self, i: int, j: int, table: np.ndarray):
+        # table[i, j] 必须是数字
+        assert(table[i, j].isdigit())
+
+        coordinates = []
+        found_mines = 0
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                ni = i + dx
+                nj = j + dy
+                if ni < 0 or ni >= table.shape[0] or nj < 0 or nj >= table.shape[1]:
+                    continue
+
+                if table[ni, nj] == 'mine':
+                    found_mines += 1
+                if table[ni, nj] == 'unknown':
+                    coordinates.append((ni, nj))
+
+        return Constraint(coordinates), int(table[i, j]) - found_mines
+
+    def init(self):
+        self.refresh_table(refresh_by_screenshot=True)
+        self.mine_count = self.mine_total
+        
+    def deduce_table_with_assumptions(self, try_count: int = 5):
+        # 刷新一次 Table
+        self.refresh_table(refresh_by_screenshot=False)
+
+        for _ in range(try_count):
+            try:
+                constraints = self.create_table_constraints(self.table, self.mine_count)
+            except:
+                return False
+
+            mine_marked, safe_marked = set(), set()
+            
+            # 1. 检查是否有哪个数字可以直接告诉我们信息
+            try:
+                new_mine_marked, new_safe_marked = self.solve_by_ensure(constraints)
+                mine_marked.update(new_mine_marked)
+                safe_marked.update(new_safe_marked)
+            except:
+                # 发生错误，那么就说明这个假设不应该存在，是错误
+                return False
+
+            # 2. 检查交叠区域是否可以有确定信息
+            if (len(mine_marked) == 0 and len(safe_marked) == 0):
+                new_mine_marked, new_safe_marked = self.solve_by_intersect(constraints)
+                mine_marked.update(new_mine_marked)
+                safe_marked.update(new_safe_marked)
+
+            # 不进行点击，也不进行刷新。只是更新是否有雷或者是否安全
+            for coordinate in mine_marked:
+                self.table[coordinate[0], coordinate[1]] = 'mine'
+
+            # 由于我们是在推测，所以只知道这里不是雷，没办法真正鼠标点击它查看具体的数字，所以这里标记为 question
+            for coordinate in safe_marked:
+                self.table[coordinate[0], coordinate[1]] = 'question'
+
+            self.mine_count -= len(mine_marked)
+            self.unknown_count -= (len(safe_marked) + len(mine_marked))
+
+            # 退出条件：雷 = 0，unkown = 0
+            if self.mine_count == 0 and self.unknown_count == 0:
+                break
+
+        return True
+
+
+    def solve(self, rounds: int = 200):
+        for i in range(rounds):
+            print(f'处理第 {i} 个表格.......................')
+
+            self.init()
+            is_done = False
+            for _ in range(40):
+                constraints = self.create_table_constraints(self.table, self.mine_count)
+
+                mine_marked, safe_marked = set(), set()
+                
+                # 1. 检查是否有哪个数字可以直接告诉我们信息
+                new_mine_marked, new_safe_marked = self.solve_by_ensure(constraints)
+                mine_marked.update(new_mine_marked)
+                safe_marked.update(new_safe_marked)
+
+                # 2. 检查交叠区域是否可以有确定信息
+                if (len(mine_marked) == 0 and len(safe_marked) == 0):
+                    new_mine_marked, new_safe_marked = self.solve_by_intersect(constraints)
+                    mine_marked.update(new_mine_marked)
+                    safe_marked.update(new_safe_marked)
+
+                # 3. 如果不可以，那就需要穷举遍历了
+                if (len(mine_marked) == 0 and len(safe_marked) == 0):
+                    new_mine_marked, new_safe_marked = self.solve_by_force(constraints)
+                    mine_marked.update(new_mine_marked)
+                    safe_marked.update(new_safe_marked)
+
+                for coordinate in mine_marked:
+                    print(f'Mine: {coordinate}')
+                    self.table[coordinate[0], coordinate[1]] = 'mine'
+                    self.window_analyzer.click_cell(coordinate[0], coordinate[1], 'right')
+
+                for coordinate in safe_marked:
+                    print(f'Safe: {coordinate}')
+                    self.window_analyzer.click_cell(coordinate[0], coordinate[1], 'left')
+
+                self.mine_count -= len(mine_marked)
+                self.unknown_count -= (len(safe_marked) + len(mine_marked))
+
+                # 退出条件：雷 = 0，unkown = 0
+                if self.mine_count == 0 and self.unknown_count == 0:
+                    is_done = True
+                    break
+
+                if len(safe_marked) > 0:
+                    self.refresh_table()
+
+                self.print_table(self.table)
+            
+            if is_done:
+                self.window_analyzer.click_goto_next_level()
+            else:
+                self.window_analyzer.click_skip_this_level()
+
+
+    def solve_by_ensure(self, constraints: dict) -> tuple[set, set]:
+        new_safe_marked = set()
+        new_mine_marked = set()
+        for coordinates, mine_count in constraints.items():
+            if len(coordinates) == mine_count:
+                for coordinate in coordinates:
+                    new_mine_marked.add(coordinate)
+
+            if mine_count == 0:
+                for coordinate in coordinates:
+                    new_safe_marked.add(coordinate)
+
+            # 如果小于，那么报错
+            if len(coordinates) < mine_count:
+                raise ValueError(f'len(coordinates) < mine_count: {len(coordinates)} < {mine_count}')
+            if mine_count < 0:
+                raise ValueError(f'mine_count < 0: {mine_count}')
+
+        return (new_mine_marked, new_safe_marked)
+                
+
+    def solve_by_intersect(self, constraints: dict):
+        new_mine_marked = set()
+        new_safe_marked = set()
+
+        for now_coordinates, now_mine_count in constraints.items():
+            for other_coordinates, other_mine_count in constraints.items():
+                new_coordinates = other_coordinates - now_coordinates
+                new_mine_count = other_mine_count - now_mine_count
+
+                # 这里不应该等于 0，等于 0 意味着 []，即两个集合相等
+                if len(new_coordinates) == new_mine_count and new_mine_count > 0:
+                    for coordinate in new_coordinates:
+                        new_mine_marked.add(coordinate)
+
+                    # 这里提前返回，因为两重循环复杂度太高了
+                    return new_mine_marked, new_safe_marked
+        
+        return new_mine_marked, new_safe_marked
+
+    def solve_by_force(self, constraints: dict):
+        scores = []
+        coordinates_list = []
+        for coordinates, mine_count in constraints.items():
+            if len(coordinates) < 10:
+                score = math.comb(len(coordinates), mine_count)
+                scores.append(score)
+                coordinates_list.append(coordinates)
+
+        # 排序，优先选择 scores 最低的
+        idx = sorted(range(len(scores)), key=lambda i: scores[i])
+        coordinates_list = [coordinates_list[i] for i in idx]
+        scores = [scores[i] for i in idx]
+
+        # 每次尝试去暴力遍历，直到找到一个确定解
+        for i in range(len(coordinates_list)):
+            print('尝试暴力遍历:', coordinates_list[i], scores[i])
+
+            # 临时把 print 关掉
+            sys.stdout = open(os.devnull, 'w')
+            sys.stderr = open(os.devnull, 'w')
+
+            coordinates = coordinates_list[i]
+            mine_count = constraints[coordinates]
+
+            table_bak = self.table.copy()
+
+            # 从 coordinates 中随机选择 mine_count 个坐标，设置为 mine
+            tables = []
+            for marked_mine_coordinates in itertools.combinations(coordinates, mine_count):
+                self.table = table_bak.copy()
+                for coordinate in marked_mine_coordinates:
+                    self.table[coordinate[0], coordinate[1]] = 'mine'
+                # 尝试五次分析
+                is_ok = self.deduce_table_with_assumptions(try_count=5)
+                if is_ok:
+                    tables.append(self.table.copy())
+
+            new_mine_marked = set()
+            new_safe_marked = set()
+
+            self.table = table_bak.copy()
+            for i in range(self.table.shape[0]):
+                for j in range(self.table.shape[1]):
+                    if self.table[i, j] == 'unknown':
+                        # 检查 tables 中是否全部是 mine
+                        is_all_mine = True
+                        for table in tables:
+                            if table[i, j] != 'mine':
+                                is_all_mine = False
+                                break
+                        if is_all_mine:
+                            new_mine_marked.add((i, j))
+                            continue
+
+                        is_all_safe = True
+                        for table in tables:
+                            if table[i, j] != 'question':
+                                is_all_safe = False
+                                break
+                        if is_all_safe:
+                            new_safe_marked.add((i, j))
+                            continue
+            
+            # 恢复 print
+            sys.stdout = sys.__stdout__
+            sys.stderr = sys.__stderr__
+
+            # 恢复现场 
+            self.table = table_bak.copy()
+            self.refresh_table(refresh_by_screenshot=False)
+
+            print(f'本次遍历结果: mine={new_mine_marked}, safe={new_safe_marked}')
+
+            if len(new_mine_marked) > 0 or len(new_safe_marked) > 0:
+                return (new_mine_marked, new_safe_marked)
+
+        return (set(), set())
+
+    def solve_one(self):
+        self.refresh_table(refresh_by_screenshot=False)
+        self.print_table(self.table)
+
+        is_done = False
+        for _ in range(40):
+            constraints = self.create_table_constraints(self.table, self.mine_count)
+
+            mine_marked, safe_marked = set(), set()
+            
+            # 1. 检查是否有哪个数字可以直接告诉我们信息
+            new_mine_marked, new_safe_marked = self.solve_by_ensure(constraints)
+            mine_marked.update(new_mine_marked)
+            safe_marked.update(new_safe_marked)
+
+            # 2. 检查交叠区域是否可以有确定信息
+            if (len(mine_marked) == 0 and len(safe_marked) == 0):
+                new_mine_marked, new_safe_marked = self.solve_by_intersect(constraints)
+                mine_marked.update(new_mine_marked)
+                safe_marked.update(new_safe_marked)
+
+            # 3. 如果不可以，那就需要穷举遍历了
+            if (len(mine_marked) == 0 and len(safe_marked) == 0):
+                new_mine_marked, new_safe_marked = self.solve_by_force(constraints)
+                mine_marked.update(new_mine_marked)
+                safe_marked.update(new_safe_marked)
+
+            for coordinate in mine_marked:
+                print(f'Mine: {coordinate}')
+                self.table[coordinate[0], coordinate[1]] = 'mine'
+                self.window_analyzer.click_cell(coordinate[0], coordinate[1], 'right')
+
+            for coordinate in safe_marked:
+                print(f'Safe: {coordinate}')
+                self.window_analyzer.click_cell(coordinate[0], coordinate[1], 'left')
+
+            self.mine_count -= len(mine_marked)
+            self.unknown_count -= (len(safe_marked) + len(mine_marked))
+
+            # 退出条件：雷 = 0，unkown = 0
+            if self.mine_count == 0 and self.unknown_count == 0:
+                is_done = True
+                break
+
+            if len(safe_marked) > 0:
+                self.refresh_table()
+
+            self.print_table(self.table)
+        
+        if is_done:
+            self.window_analyzer.click_goto_next_level()
+        else:
+            self.window_analyzer.click_skip_this_level()
+
+
+
+if __name__ == "__main__":
+    weeper = Weeper(None, mine_total=26)
+    weeper.solve(2)
+
+    # table = np.array([
+    #     ['unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'],
+    #     ['unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'],
+    #     ['unknown', 'unknown', 'unknown', 'unknown', 'unknown', 2,         'unknown'],
+    #     [1,         'unknown', 'unknown', 'unknown', 'unknown', 4,         'unknown'],
+    #     ['unknown', 'unknown', 4,          3,        'unknown', 'unknown', 'unknown'],
+    #     ['unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown'],
+    #     ['unknown', 'unknown', 1,         'unknown', 'unknown', 'unknown', 'unknown'],
+    # ])
+
+    # weeper = Weeper(table, 20)
+    # weeper.solve_one()
---
+++ window_analyzer.py
@@ -0,0 +1,225 @@
+"""
+使用 pywinauto 分析窗口结构的简单程序
+"""
+import os
+import time
+import pywinauto
+from pywinauto import Application
+import numpy as np
+import cv2
+from PIL import Image
+from typing import Tuple, List, Dict, Optional
+
+class WindowAnalyzer:
+    def __init__(self, title: str):
+        self.title = title
+
+        # 窗口左上角位置
+        self.win_left_top = None
+        # 表格左上角位置
+        self.table_left_top = None
+        # 单元格大小
+        self.cell_w = None
+        self.cell_h = None
+
+        app = Application(backend="uia").connect(title=self.title)
+        self.window = app.window(title=self.title)
+
+        # 获取窗口在屏幕上左上角的位置
+        window_rect = self.window.rectangle()
+        self.win_left_top = [window_rect.left, window_rect.top]
+        self.window_width = window_rect.right - window_rect.left
+        self.window_height = window_rect.bottom - window_rect.top
+
+
+    def capture_window_screenshot(self, save_path=None):
+        """
+        对指定窗口进行截图，返回截图和窗口对象
+        返回: (screenshot, window, window_rect)
+        window_rect: (x, y, width, height) 窗口在屏幕上的位置
+        """
+        try:
+            # 截图前，需要先移动到屏幕左上角，防止鼠标影响表格
+            temp_col = int(self.win_left_top[0]+self.window_width*0.2)
+            temp_row = int(self.win_left_top[1]+self.window_height*0.2)
+            pywinauto.mouse.click(coords=(temp_col, temp_row))
+            time.sleep(0.2)
+            pywinauto.mouse.click(coords=(temp_col, temp_row))
+            
+            print(f"正在截取窗口: {self.title}")
+            screenshot = self.window.capture_as_image()
+            if save_path:
+                screenshot.save(save_path)
+
+            return screenshot
+
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            print(f"截图失败: {str(e)}")
+            return None
+
+    def click_skip_this_level(self):
+        clicked_x = int(self.window_height * 0.14) + self.win_left_top[1]
+        clicked_y = int(self.window_width * 0.82) + self.win_left_top[0]
+        pywinauto.mouse.move(coords=(clicked_y, clicked_x))
+        time.sleep(0.2)
+        pywinauto.mouse.click(coords=(clicked_y, clicked_x))
+        time.sleep(0.2)
+
+        clicked_x = int(self.window_height * 0.79) + self.win_left_top[1]
+        clicked_y = int(self.window_width * 0.68) + self.win_left_top[0]
+        pywinauto.mouse.move(coords=(clicked_y, clicked_x))
+        time.sleep(0.2)
+        pywinauto.mouse.click(coords=(clicked_y, clicked_x))
+        time.sleep(0.2)
+
+
+    def click_goto_next_level(self):
+        # screenshot = self.capture_window_screenshot()
+        # screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+        # rows, cols = screenshot.shape[0:2]
+        # screenshot[int(rows*0.75):int(rows*0.8), int(cols*0.68):int(cols*0.7)] = [255, 0, 0]
+        # cv2.imwrite("screenshot.png", screenshot)
+
+        # TODO: 现在先强制固定的偏移位置
+        clicked_x = int(self.window_height * 0.77) + self.win_left_top[1]
+        clicked_y = int(self.window_width * 0.55) + self.win_left_top[0]
+
+        pywinauto.mouse.move(coords=(clicked_y, clicked_x))
+        time.sleep(0.2)
+        pywinauto.mouse.click(coords=(clicked_y, clicked_x))
+        time.sleep(0.2)
+
+
+    def parse_img_to_table(self, screenshot: Image.Image) -> np.ndarray:
+        """
+        从图像中解析表格。
+        """
+        screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+
+        rows, cols = screenshot.shape[0:2]
+        self.table_left_top = [cols*2//10, rows*2//10]
+        self.table_right_bottom = [cols*8//10, rows*95//100]
+        tableimg = screenshot[
+            self.table_left_top[1]:self.table_right_bottom[1], 
+            self.table_left_top[0]:self.table_right_bottom[0]
+        ]
+        cv2.imwrite("table.png", tableimg)
+
+        '''
+        边界提取，本来是打算做直线检测等复杂算法，但是想想算了...
+        更简单的方法：直接从上往下、从左往右扫，如果连续一片都是直线那就说明是框...
+        '''
+        rows, cols = tableimg.shape[0:2]
+
+        # 从上往下：中间开始 1/4 到 3/4 之间
+        row_border = []
+        prev_is_border = False
+        for i in range(rows):
+            is_border = np.all(tableimg[i, int(cols*0.4):int(cols*0.6), 1] > 200)
+            if is_border and not prev_is_border:
+                row_border.append(i)
+            prev_is_border = is_border
+        
+        # 从左往右：上下 1/4 到 3/4 之间
+        col_border = []
+        prev_is_border = False
+        for j in range(cols):
+            is_border = np.all(tableimg[int(rows*0.4):int(rows*0.6), j, 1] > 200)
+            if is_border and not prev_is_border:
+                col_border.append(j)
+            prev_is_border = is_border
+
+        self.table_left_top = [self.table_left_top[0] + col_border[0], self.table_left_top[1] + row_border[0]]
+        self.cell_w, self.cell_h = col_border[1] - col_border[0], row_border[1] - row_border[0]
+
+        '''
+        每个单元格解析
+        '''
+        table_data = np.empty((len(row_border) - 1, len(col_border) - 1), dtype=object)
+        for i in range(len(row_border) - 1):
+            for j in range(len(col_border) - 1):
+                cell_img = tableimg[row_border[i]:row_border[i+1], col_border[j]:col_border[j+1]]
+                cell_img = cell_img[int(self.cell_h*0.1):int(self.cell_h*0.9), int(self.cell_w*0.1):int(self.cell_w*0.9)]
+                # cv2.imwrite(f'cell_{i}_{j}.png', cell_img)
+                table_data[i, j] = self._check_cell_data(cell_img)
+        return table_data
+
+    def click_cell(self, i, j, left_or_right):
+        row = self.win_left_top[1] + self.table_left_top[1] + int((i+0.5) * self.cell_h)
+        col = self.win_left_top[0] + self.table_left_top[0] + int((j+0.5) * self.cell_w)
+        pywinauto.mouse.move(coords=(col, row))
+        time.sleep(0.2)
+        pywinauto.mouse.click(coords=(col, row), button=left_or_right)
+
+    def _check_cell_data(self, cell_img: np.ndarray) -> str:
+        assert(len(cell_img.shape) == 3)
+
+        # 检查是否是空，判断：全部为黑
+        if np.all(cell_img[:, :, 1] < 32):
+            return 'unknown'
+
+        # 遍历 Template
+        is_first = True
+        best, best_score = None, -1
+        for fname in os.listdir('templates'):
+            template = cv2.imread(f'templates/{fname}')
+            if is_first:
+                matched = cv2.resize(cell_img, (template.shape[1], template.shape[0]))
+                is_first = False
+
+            inter = np.logical_and(matched, template).sum()
+            union = np.logical_or(matched, template).sum()
+            score = inter / union
+            if score > best_score:
+                best, best_score = fname, score
+
+        return best[:best.rfind('.')]
+
+        # # 检查是否是空，判断：全部为黑
+        # if np.all(cell_img[:, :, 1] < 32):
+        #     return 'Unknown'
+
+        # # 检查是否是雷，判断：存在少量黄色
+        # if np.sum((cell_img[:, :, 2] < 10) & (cell_img[:, :, 1] > 200)) > 10:
+        #     return 'Mine'
+        
+        # # 检查是否是问号，判断：存在少量灰色
+        # if np.sum((cell_img[:, :, 1] < 132) & (cell_img[:, :, 1] > 100)) > 10:
+        #     return 'Question'
+
+        # # 否则就是数字
+        # import os
+        # import pytesseract
+        # pytesseract_dir = r'./Tesseract-3'
+        # pytesseract.pytesseract.tesseract_cmd = os.path.join(pytesseract_dir, 'tesseract.exe')
+
+        # image = Image.open('./test.png')
+        # image = image.crop((500, 490, 600, 540))
+        # image = Image.open('./gray.png')
+        # image = image.convert('L')
+        # ratio = 300 // min(image.size)
+        # new_size = (image.size[0]*ratio, image.size[1]*ratio)
+        # image = image.resize(new_size, Image.Resampling.LANCZOS)
+        # image.save('./edit.png')
+
+        # text = pytesseract.image_to_string(cell_img, config=f'{pytesseract_dir}\\tessdata\\configs\\digits').strip()
+        # return text
+            
+
+
+if __name__ == "__main__":
+    print("Pywinauto 窗口分析工具")
+    print("=" * 50)
+    
+    # 使用示例：分析指定窗口
+    # 请修改下面的窗口标题为您想要分析的窗口
+    window_title = "Minesweeper Variants"  # 修改为您要分析的窗口标题
+    
+    analyzer = WindowAnalyzer(window_title)
+    # analyzer.analyze_window_by_title()
+    # screenshot = analyzer.capture_window_screenshot()
+    # tableimg = analyzer.parse_img_to_table(screenshot)
+    analyzer.click_goto_next_level()
+    # analyzer.click_skip_this_level()
